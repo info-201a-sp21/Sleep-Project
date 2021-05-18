07b7eaf5fa8bae5322f6cc5191780d51e5dbc03e
EvanK <evankun2001@gmail.com>
2021-05-17 22:51:55 -1000

    removed extraneous lines of code

--- scripts/types of days vs avg time slept.R
+++ scripts/types of days vs avg time slept.R
@@ -9,18 +9,9 @@ sleep_data <- read.csv(
   stringsAsFactors = FALSE
 )
 
-mean_hours <- sleep_data %>%
-  summarize(Avg.hrs.per.day.sleeping)
+sample_data <- sample_n(sleep_data, 200) 
 
-ggplot(data = sleep_data) +
-  geom_violin(mapping = aes(x = Avg.hrs.per.day.sleeping, y = Type.of.Days)) +
-  ggtitle("Types of Days vs. Average Hours Slept per Day") + 
-  xlab("Average Hours Slept per Day") +
-  ylab("Types of Days") + 
-  geom_jitter(shape = 5, position = position_jitter(0.2))
-
-
-mean_plot <- ggplot(data = sleep_data, 
+mean_plot <- ggplot(data = sample_data, 
   mapping = aes(x = Avg.hrs.per.day.sleeping, y = Type.of.Days)
 ) +
   geom_violin(trim = FALSE) +
@@ -28,7 +19,7 @@ mean_plot <- ggplot(data = sleep_data,
   xlab("Average Hours Slept per Day") +
   ylab("Types of Days") 
 
-mean_plot +
+plot_with_points <- mean_plot +
   geom_jitter(shape = 16, position = position_jitter(0))
 
 
