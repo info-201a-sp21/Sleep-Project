729abcd9861f785623c1fde4c1dfbea2e58f7c93
Woojin Kil <peperojyj25@gmail.com>
2021-05-19 11:16:06 -0700

    Made some changes in trend line

--- scripts/trend of avg sleep hr in each age group.R
+++ scripts/trend of avg sleep hr in each age group.R
@@ -1,8 +1,26 @@
 
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+
+sleep_df <- read.csv("data/Time Americans Spend Sleeping.csv",
+                     stringsAsFactors = FALSE)
+
 get_trend_line_plot <- function(dataset) {
-  
+
   plot <- ggplot(data = dataset) +
     geom_smooth(mapping = aes(x = Year, y = Avg.hrs.per.day.sleeping,
-                              color = Age.Group), se = FALSE)
-  return(plot)
+                              color = Age.Group), se = FALSE) +
+    labs(
+      title = "Trend of Average Sleep Hour In Each Age Group",
+      x = "Year",
+      y = "Average Hours per Day Sleeping",
+      color = "Age Group"
+    )
+
+  interactive_plot <- ggplotly(plot)
+  return(interactive_plot)
 }
+
+
