5fbd22b18b7a2a20f0bb85067b224a09dd05045f
celine chong <cjchong@uw.edu>
2021-05-20 16:45:36 -0700

    run lintr

--- scripts/avg time slept pie chart.R
+++ scripts/avg time slept pie chart.R
@@ -14,18 +14,18 @@ avg_hours_df <- avg_time_slept %>%
   mutate(sleep_buckets = floor(Avg.hrs.per.day.sleeping))
 
 #Building the actual pie chart
-avg_sleep_chart <- ggplot(avg_hours_df, 
+avg_sleep_chart <- ggplot(avg_hours_df,
   aes(x = factor(1), fill = factor(sleep_buckets))) +
   geom_bar(width = 1) +
   coord_polar(theta = "y")
 
 #Fixing the visual aspects of the pie chart
 avg_sleep_pie <- avg_sleep_chart +
-  scale_fill_manual(values = c("#785EF0", "#DC267F", "#FE6100", "#FFB000"), 
-        name =" Hours of Sleep (rounded to nearest hour)",
+  scale_fill_manual(values = c("#785EF0", "#DC267F", "#FE6100", "#FFB000"),
+        name = " Hours of Sleep (rounded to nearest hour)",
         labels = c("7 Hours", "8 Hours", "9 Hours", "10 Hours")) +
   ggtitle("Hours American Spend Sleeping") +
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.title.y=element_blank(),
-        axis.text.y=element_blank())
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank())
