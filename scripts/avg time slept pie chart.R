7c9448075ad2f50884ff181ff55f2d0d931badba
celine chong <cjchong@uw.edu>
2021-05-20 16:39:34 -0700

    rename file

--- scripts/avg time slept pie chart.R
+++ scripts/avg time slept pie chart.R
@@ -1,3 +1,4 @@
+#loading csv data
 avg_time_slept <- read.csv(
   "https://raw.githubusercontent.com/info-201a-sp21/Sleep-Project
 /master/data/Time%20Americans%20Spend%20Sleeping.csv",
@@ -6,39 +7,25 @@ avg_time_slept <- read.csv(
 
 library("dplyr")
 library("ggplot2")
-library("leaflet")
-library("plotly")
+library("lintr")
 
-#data I'm working with
-avg_hours_slept <- select(avg_time_slept, Avg.hrs.per.day.sleeping)
-
-#rounding the data
-seven_hours <- avg_time_slept[avg_time_slept$Avg.hrs.per.day.sleeping >= 7 & 
-                                avg_time_slept$Avg.hrs.per.day.sleeping <= 7.999,]
-seven_hrs_perc <- (nrow(seven_hours) / nrow(avg_hours_slept)) * 100
-
-eight_hours <- avg_time_slept[avg_time_slept$Avg.hrs.per.day.sleeping >= 8 & 
-                                avg_time_slept$Avg.hrs.per.day.sleeping <= 8.999,]
-eight_hrs_perc <- (nrow(eight_hours) / nrow(avg_hours_slept)) * 100
-
-nine_hours <- avg_time_slept[avg_time_slept$Avg.hrs.per.day.sleeping >= 9 & 
-                               avg_time_slept$Avg.hrs.per.day.sleeping <= 9.999,]
-nine_hrs_perc <- (nrow(nine_hours) / nrow(avg_hours_slept)) * 100
-
-ten_hours <- avg_time_slept[avg_time_slept$Avg.hrs.per.day.sleeping >= 10 & 
-                              avg_time_slept$Avg.hrs.per.day.sleeping <= 10.999,]
-ten_hrs_perc <- (nrow(seven_hours) / nrow(avg_hours_slept)) * 100
-
-#making  the dataframe
-Hours_slept <- c("seven hours" = seven_hrs_perc, "eight hours" = eight_hrs_perc, 
-                 "nine hours" = nine_hrs_perc, "ten hours" = ten_hrs_perc)
-
-
+#Creating a column for my specific data
 avg_hours_df <- avg_time_slept %>%
   mutate(sleep_buckets = floor(Avg.hrs.per.day.sleeping))
 
-avg_sleep_pie <- ggplot(avg_hours_df, aes(x = factor(1), fill = factor(sleep_buckets))) +
+#Building the actual pie chart
+avg_sleep_chart <- ggplot(avg_hours_df, 
+  aes(x = factor(1), fill = factor(sleep_buckets))) +
   geom_bar(width = 1) +
   coord_polar(theta = "y")
 
-  
+#Fixing the visual aspects of the pie chart
+avg_sleep_pie <- avg_sleep_chart +
+  scale_fill_manual(values = c("#785EF0", "#DC267F", "#FE6100", "#FFB000"), 
+        name =" Hours of Sleep (rounded to nearest hour)",
+        labels = c("7 Hours", "8 Hours", "9 Hours", "10 Hours")) +
+  ggtitle("Hours American Spend Sleeping") +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank())
