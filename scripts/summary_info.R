cc596693fc7006046a9ffd089ed68ea50b8a055f
Woojin Kil <peperojyj25@gmail.com>
2021-05-20 06:22:21 -0700

    modified summary info

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,27 +1,94 @@
+library(dplyr)
+library(ggplot2)
+
+sleep_df <- read.csv("data/Time Americans Spend Sleeping.csv",
+                     stringsAsFactors = FALSE)
+
+sleep_study_df <- read.csv("data/SleepStudyData.csv",
+                           stringsAsFactors = FALSE)
 
 
 get_summary_info <- function(dataset) {
-  sleep_df <- read.csv("data/Time Americans Spend Sleeping.csv",
-                       stringsAsFactors = FALSE)
   result <- list()
-  
-  sleep_df %>% group_by(Sex) %>%
-    group_by(Age.Group) %>%
+
+  phoneuser_info <- sleep_study_df %>% filter(PhoneTime == "Yes") %>%
+    mutate(answer = as.numeric(Enough == "Yes")) %>%
     summarize(
-      average_hrs_in_particular_sex_age = mean(Avg.hrs.per.day.sleeping)
+      avg_sleep_hr = mean(Hours, na.rm = TRUE),
+      avg_enough = sum(answer, na.rm = TRUE) / 
+        length(!is.na(Enough))
     )
   
+   non_phoneuser_info <- sleep_study_df %>% filter(PhoneTime == "No") %>%
+    mutate(answer = as.numeric(Enough == "Yes")) %>%
+    summarize(
+      avg_sleep_hr = mean(Hours, na.rm = TRUE),
+      avg_enough = sum(answer, na.rm = TRUE) /
+        length(!is.na(Enough))
+    )
+   
+  breakfast_info <- sleep_study_df %>% filter(Breakfast == "Yes") %>%
+    mutate(answer = as.numeric(Enough == "Yes")) %>%
+    summarize(
+      avg_sleep_hr = mean(Hours, na.rm = TRUE),
+      avg_enough = sum(answer, na.rm = TRUE) / 
+        length(!is.na(Enough))
+    )
   
-  sleep_df %>% filter(Sex == )
+  non_breakfast_info <- sleep_study_df %>% filter(Breakfast == "No") %>%
+    mutate(answer = as.numeric(Enough == "Yes")) %>%
+    summarize(
+      avg_sleep_hr = mean(Hours, na.rm = TRUE),
+      avg_enough = sum(answer, na.rm = TRUE) /
+        length(!is.na(Enough))
+    )
+ 
+  result$avg_enough_sleephr <- sleep_study_df %>%
+    filter(Enough == "Yes") %>%
+    summarize(
+      avg_enough_sleep_hr = mean(Hours, na.rm = TRUE)
+    ) %>% pull()
+  
+  avg_phoneuser_sleep_hr <- phoneuser_info %>% pull(avg_sleep_hr)
+  avg_non_phoneuser_sleep_hr <- non_phoneuser_info %>%
+    pull(avg_sleep_hr)
   
   
+  avg_phoneuser_enough_rate <- phoneuser_info %>% pull(avg_enough)
+  avg_non_phoneuser_enough_rate <- non_phoneuser_info %>%
+    pull(avg_enough)
   
+  result$avg_phoneuser_sleep_hr <- round(avg_phoneuser_sleep_hr, 3)
+  result$avg_non_phoneuser_sleep_hr <- round(avg_non_phoneuser_sleep_hr, 3)
+  
+  result$avg_phoneuser_enough_rate <- round(avg_phoneuser_enough_rate, 3)
+  result$avg_non_phoneuser_enough_rate <- round(
+    avg_non_phoneuser_enough_rate, 3)
+  
+  
+  avg_breakfast_sleep_hr <- breakfast_info %>% pull(avg_sleep_hr)
+  avg_non_breakfast_sleep_hr <- non_breakfast_info %>%
+    pull(avg_sleep_hr)
+  
+  avg_breakfast_enough_rate <- breakfast_info %>% pull(avg_enough)
+  avg_non_breakfast_enough_rate <- non_breakfast_info %>%
+    pull(avg_enough)
+  
+  result$avg_breakfast_sleep_hr <- round(avg_breakfast_sleep_hr, 3)
+  result$avg_non_breakfast_sleep_hr <- round(avg_non_breakfast_sleep_hr, 3)
+  
+  result$avg_breakfast_enough_rate <- round(avg_breakfast_enough_rate, 3)
+  result$avg_non_breakfast_enough_rate <-round(avg_non_breakfast_enough_rate, 3)
+  
+  result$breakfast_sleephr_ratio <- round(result$avg_breakfast_sleep_hr /
+    result$avg_non_breakfast_sleep_hr, 2)
+  
+  result$breakfast_enough_ratio <- round(result$avg_breakfast_enough_rate /
+    result$avg_non_breakfast_enough_rate, 2)
+  
+  return(result)
+
 }
 
+get_summary_info(sleep_df)
 
-sleep_df %>% group_by(Sex)
-
-ggplot(data = sleep_df) +
-  geom_point(mapping = aes(x = Sex, y = Avg.hrs.per.day.sleeping,
-                           color = Avg.hrs.per.day.sleeping),
-             position = "jitter")
