f21cc61c3add3af3c11a68e335f95350235aaa1e
Matt Chang <MattChang@Matts-MacBook-Air-2.local>
2021-05-20 15:55:26 -0700

    summary table function added

--- scripts/aggregate_table.R
+++ scripts/aggregate_table.R
@@ -1,1 +1,13 @@
+library("dplyr")
+
+#summary table grouped by age and year
+summary_table <- function(df){
+  aggregate_df <- df %>%
+    select(Year, Avg.hrs.per.day.sleeping, Age.Group) %>%
+    group_by(Age.Group, Year) %>%
+    summarize(count = n(),
+              avg_sleep = mean(Avg.hrs.per.day.sleeping)
+    )
+  return(aggregate_df)
+}
 
