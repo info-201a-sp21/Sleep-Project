2f0a488fadf9a9bc02f904a8cb28e562c35b0ac1
Woojin Kil <peperojyj25@gmail.com>
2021-05-20 13:39:03 -0700

    changed the smooth curve graph to line graph

--- scripts/trend of avg sleep hr in each age group.R
+++ scripts/trend of avg sleep hr in each age group.R
@@ -3,13 +3,21 @@ library(dplyr)
 library(ggplot2)
 library(plotly)
 
+
 get_trend_line_plot <- function(dataset) {
 
-  plot <- ggplot(data = dataset) +
-    geom_smooth(mapping = aes(x = Year, y = Avg.hrs.per.day.sleeping,
-                              color = Age.Group), se = FALSE) +
+  trend_of_avg_hrs_per_age_group <- dataset %>%
+    group_by(Year, Age.Group) %>%
+    summarize(
+      avg_sleep_hr_per_age_group_per_year = mean(Avg.hrs.per.day.sleeping)
+    )
+
+  plot <- ggplot(data = trend_of_avg_hrs_per_age_group) +
+    geom_line(mapping = aes(x = Year,
+                            y = avg_sleep_hr_per_age_group_per_year,
+                            color = Age.Group)) +
     labs(
-      title = "Trend of Average Sleep Hour In Each Age Group",
+      title = "Trend of Average Sleep Hours In Each Age Group",
       x = "Year",
       y = "Average Hours per Day Sleeping",
       color = "Age Group"
