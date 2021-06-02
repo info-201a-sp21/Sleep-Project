d0581d5fa9dbdb0d238fa26ff4f3e061fc272527
Woojin Kil <peperojyj25@gmail.com>
2021-06-02 12:43:02 -0700

    added page 3 contents in app_ui.R file

--- app_ui.R
+++ app_ui.R
@@ -1,3 +1,7 @@
+library(plotly)
+library(shiny)
+
+
 page_2_side <- sidebarPanel(
   selectInput(
     "scattervar",
@@ -20,6 +24,46 @@ page_2 <- tabPanel(
   sidebarLayout(
     page_2,
     page_2_main
+  )
+)
+
+page_3_side <- sidebarPanel(
+  p(
+  "The interactive graph to the right depict the trend of average sleep
+    hours in each age group and overall is set to default for comparing
+    those trends between age groups. Hover to see the exact average sleep
+    hours in each year"),
+  p(
+    "You can seee the sleep hours trend for a specific age group by selecting
+    an age group category below."
+  ),
+  selectInput(
+    inputId = "age_cate",
+    label = "Select Age Category",
+    choices = list(
+      "overall" = "overall",
+      "15 years and over" = "15 years and over",
+      "15 ~ 24 years" = "15 to 24 years",
+      "25 ~ 34 years" = "25 to 34 years",
+      "35 ~ 44 years" = "35 to 44 years",
+      "45 ~ 54 years" = "45 to 54 years",
+      "55 ~ 64 years" = "55 to 64 years",
+      "65+ years" = "65 years and over"
+    ),
+    selected = "overall"
+  )
+)
+
+page_3_main <- mainPanel(
+  plotlyOutput("show_trend")
+)
+
+page_3 <- tabPanel(
+  "Average Sleep Hour Trends per Age Group",
+  h2("Trend of Average Sleep Hours In Each Age Group"),
+  sidebarLayout(
+    page_3_side,
+    page_3_main
   )
 )
 
