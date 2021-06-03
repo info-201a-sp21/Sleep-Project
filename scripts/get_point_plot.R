66db070d659e6b8e6247b8a63ed454d30754c68b
EvanK <evankun2001@gmail.com>
2021-06-02 23:10:11 -1000

    made scatter plot fully interactable

--- scripts/get_point_plot.R
+++ scripts/get_point_plot.R
@@ -4,19 +4,19 @@ library("plotly")
 
 get_point_plot <- function(dataset, dataCategory) {
   
-  if (dataCategory != "averages") {
+  if (dataCategory == "w_and_h") {
     different_categories <- dataset %>%
-      filter(Type.of.Days == dataCategory)
+      filter(Type.of.Days == "Weekend days and holidays")
     
     plot1 <- ggplot(data = different_categories, mapping = aes(
       x = Avg.hrs.per.day.sleeping,
       y = Type.of.Days 
     )) + 
       labs(
-      title = paste0("Types of Days vs. Average Hours Slept per Day: ",
-                     dataCategory),
-      x = "Average Hours Slept per Day",
-      y = "Types of Days"
+        title = paste0("Types of Days vs. Average Hours Slept per Day:
+                     Weekend Days and Holidays"),
+        x = "Average Hours Slept per Day",
+        y = "Types of Days"
     )
 
   plot_with_points1 <- plot1 +
@@ -26,10 +26,55 @@ get_point_plot <- function(dataset, dataCategory) {
   interactive_point1 <- ggplotly(plot_with_points1)
   return(interactive_point1)
   
+  } else if (dataCategory == "n_w") {
+    different_categories <- dataset %>%
+      filter(Type.of.Days == "Nonholiday weekdays")
+    
+    plot2 <- ggplot(data = different_categories, mapping = aes(
+      x = Avg.hrs.per.day.sleeping,
+      y = Type.of.Days 
+    )) + 
+      labs(
+        title = paste0("Types of Days vs. Average Hours Slept per Day:
+                       Nonholiday Weekdays"),
+        x = "Average Hours Slept per Day",
+        y = "Types of Days"
+    )
+    
+    plot_with_points2 <- plot2 +
+      geom_jitter(shape = 16) +
+      stat_summary(fun = mean, geom = "point", shape = 16, size = 4, color =
+                     "red")
+    interactive_point2 <- ggplotly(plot_with_points2)
+    return(interactive_point2)
+
+  } else if (dataCategory == "all_days") {
+    different_categories <- dataset %>%
+      filter(Type.of.Days == "All days")
+    
+    plot3 <- ggplot(data = different_categories, mapping = aes(
+      x = Avg.hrs.per.day.sleeping,
+      y = Type.of.Days 
+    )) + 
+      labs(
+        title = paste0("Types of Days vs. Average Hours Slept per Day:
+                       Average Across all Days of The Week"),
+        x = "Average Hours Slept per Day",
+        y = "Types of Days"
+      )
+    
+    plot_with_points3 <- plot3 +
+      geom_jitter(shape = 16) +
+      stat_summary(fun = mean, geom = "point", shape = 16, size = 4, color =
+                     "red")
+    interactive_point3 <- ggplotly(plot_with_points3)
+    return(interactive_point3) 
+    
+    
   } else {
     
   
-  plot2 <- ggplot(data = dataset, mapping = aes(
+  plot4 <- ggplot(data = dataset, mapping = aes(
     x = Avg.hrs.per.day.sleeping,
     y = Type.of.Days
   )) +
@@ -39,11 +84,11 @@ get_point_plot <- function(dataset, dataCategory) {
       y = "Types of Days"
     )
   
-  plot_with_points2 <- plot +
+  plot_with_points4 <- plot4 +
     geom_jitter(shape = 16) +
     stat_summary(fun = mean, geom = "point", shape = 16, size = 4, color =
                    "red")
-  interactive_point2 <- ggplotly(plot_with_points2)
-  return(interactive_point2)
+  interactive_point4 <- ggplotly(plot_with_points4)
+  return(interactive_point4)
   }
 }
