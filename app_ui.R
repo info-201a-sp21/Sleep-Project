bff5180602cf5b282176da1bafaf07b7d7c9b81d
celine chong <cjchong@uw.edu>
2021-06-03 17:55:03 -0700

    edit conclusion

--- app_ui.R
+++ app_ui.R
@@ -179,7 +179,7 @@ page_4_main <- mainPanel(
 #Page 4 - combining main and side panel
 page_4 <- tabPanel(
   "Hours American Spend Sleeping",
-  h1("How many Hours American Spend Sleeping on Average? (Year 2003 to 2017)"),
+  h1("How Many Hours American Spend Sleeping on Average? (Year 2003 to 2017)"),
   sidebarLayout(
     page_4_side,
     page_4_main)
@@ -188,39 +188,50 @@ page_4 <- tabPanel(
 # create conclusion page
 conclusion_page <- tabPanel(
   "Takeaways",
-    h2("Takeaway 1: Hours Americans Spend Sleeping"),
+    h1("Summary Findings"),
+      h3("Sleep is an integral part of our daily function and health, 
+      but how much sleep are Americans getting? From our data, we found 
+      three major takeaways that address questions related to exploring the 
+      relationship between Americans and the average number of hours they are sleeping."
+        ),
+    h2("Takeaway 1: Respondents were, on average, able to sleep a healthy amount
+"),
+  h3("Question: What was the most common number of hours (rounded to the nearest hour) that Americans slept from 2003-2017?
+"),
       p(
-"In this chart, our research uncovered common hours Americans spent
-sleeping. We found that 8 hours and 9 hours were the most common amount of
-hours spent sleeping on average per day. However, 8 hours was the most frequent,
-accounting for over half of the data meaning generally people did not experience
-significant disturbances that impaired them from achieving a healthy amount of
-sleep. By understanding the most common amount of hours spent sleeping, we can
-potentially extrapolate this to gain a general understanding of the amount of
-time most people spend sleeping on average and thereby identify if people are
-generally able to sleep a healthy amount."
-      ),
-    h2("Takeaway 2: Types of Days vs. Hours Americans Spend Sleeping"),
+"Using the How Many Hours Americans Spend Sleeping on Average?, 
+our research uncovered common hours Americans spent sleeping. 
+We found that 8 hours and 9 hours were the most common amount of 
+hours spent sleeping on average per day with 7 hours and 10 hours being 
+the outliers in the data. However, 8 hours was the most frequent, 
+accounting for over half of the data meaning respondents generally 
+did not experience significant disturbances that impaired them from achieving 
+a healthy amount of sleep. By understanding the most common amount of hours spent 
+sleeping, we can potentially extrapolate this to gain a general understanding of 
+the amount of time most people spend sleeping on average and thereby identify if 
+people are generally able to sleep a healthy amount."),
+p("Broader Implications: With the prevalence of social media addiction and increasing technological usage from the increasing release of technological products, it may be of value to further investigate if the average hours of sleep of Americans lessened from prevalent blue-light exposure or smartphone addiction. 
+"),
+    h2("Takeaway 2: Average Hours of Sleep Changed Depending on the Type of Day"),
+h3("Question: Did the type of day affect the average number of hours slept?"),
       p(
-"From our categorical scatterplot, our data revealed large differences
-in time spent sleeping across three different categories of days. The
-visualization reveals that the average sleep for weekends was 9.33 hours,
-8.40 hours for weekdays, and 8.68 hours across all days. These results tell us
-that people change their sleeping patterns by nearly an hour from weekday to
-weekend, which can negatively affect their circadian rhythm. Changing one's
-sleep patterns can harm sleep health and the data allows us to view why some may
-be unsatisfied with their sleep."),
-  h2("Takeaway 3: Trend of Average Sleep Hours in Each Age Group"),
+"From our categorical scatterplot ("Types of Days vs. Average Hours Slept per Day"), 
+our data revealed large differences in time spent sleeping across three different categories of days. 
+The visualization reveals that the average sleep for weekends was 9.33 hours, 8.40 hours for weekdays, 
+and 8.68 hours across all days. These results tell us that people change their sleeping patterns by 
+nearly an hour from weekday to weekend, which can negatively affect their circadian rhythm. 
+Changing one's sleep patterns can harm sleep health and the data allows us to understand why some may be unsatisfied with their sleep. 
+"),
+p("Broader implications: It makes sense that Americans generally get less sleep on weekdays as compared to weekends seeing as that the workweek tends to fall on weekdays. It would be interesting to further investigate if Americans feel as though they do not get enough sleep during the weekdays which account for the longer hours slept during the weekends.
+"),
+  h2("Takeaway 3: 15-24 Age Group's Average Sleep Hours Increased the Most"),
+h3("Question: What are the different sleep patterns of the different age groups surveyed?
+"),
     p(
-"Our line graph provided insight into the sleeping patterns of different age
-groups. It revealed that except for the age group of 65 years and over, the
-average sleep hours for most of the age groups tend to rise slightly over the
-year. The age group of 65 years and over tends to stay the same while the age
-group of 15 to 24 years rises significantly compared to the other age group.
-These results help us to gain an insight that the reason for people having
-sleeping problems nowadays is not from the actual amount of sleep hours that
-they are having."
-    )
+"From our "Trend of Average Sleep Hours In Each Age Group" graph, we found insight into the sleeping patterns of different age groups. It revealed that except for the age group of 65 years and over, the average sleep hours for most of the age groups tend to rise slightly over the year. The age group of 65 years and over tends to stay the same while the age group of 15 to 24 years rises significantly compared to the other age group. These results help us to gain an insight that the reason for people having sleeping problems nowadays is not from the actual amount of sleep hours that they are having.
+"),
+p("Broader Implications: Perhaps there are issues that the 15-24 year old age groups faces that are uniquely affecting their sleep pattern. Significant life events could include entering college, graduating college, or entering the workforce. This age group's sleep patterns act like an outlier in the dataset, being significantly distanced on the graph from the rest of the age groups graphed. 
+")
 )
 
 
